54589f7ca99aa900e76ba6d4f248684b55ac2345
kwozniakowski <kwozniakowski99@gmail.com>
2020-08-13 16:09:23 +0200

    Gówno w betoniarce
    Tu powinno byc wszystko

---
+++ app/src/main/java/com/example/wbudyapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wbudyapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+    public void startGame(View view) //metoda ta musi byc publiczna, byc voidem i miec jeden argument View
+    {
+        Intent startOfGame = new Intent(this,SecondActivity.class);
+        startActivity(startOfGame);
+    }
+}
