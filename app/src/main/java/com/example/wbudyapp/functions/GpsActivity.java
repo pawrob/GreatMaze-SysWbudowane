47d9ba5fc023bd5ad0f9c7181c0ee6f0179d0513
Paweł Bucki <mrpawrob@gmail.com>
2020-08-26 19:21:10 +0200

    dodanie dystansu do pł

--- app/src/main/java/com/example/wbudyapp/functions/GpsActivity.java
+++ app/src/main/java/com/example/wbudyapp/functions/GpsActivity.java
@@ -27,6 +27,8 @@ public class GpsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gps);
         btnLoc = (Button) findViewById(R.id.btnGetLoc);
         ActivityCompat.requestPermissions(GpsActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+
         btnLoc.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
@@ -37,36 +39,16 @@ public class GpsActivity extends AppCompatActivity {
                 if( l == null){
                     Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
                 }else {
-//                    double lat = l.getLatitude();
-//                    double lon = l.getLongitude();
-//                    Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
+
                     longitude = findViewById(R.id.lat);
                     latitude = findViewById(R.id.lon);
                     distance = findViewById(R.id.dystDoPl);
                     latitude.setText("Lat: "+l.getLatitude());
                     longitude.setText("Lon: "+l.getLongitude());
-                    distance.setText("Distance: " + distanceInKmBetweenEarthCoordinates(l.getLatitude(),l.getLongitude(),51.74720764160156,19.453283309936523));
+                    distance.setText("Distance: " + gt.getDistanceToTUL(l));
                 }
             }
         });
     }
 
-    public double degreesToRadians(double degrees) {
-        return degrees * Math.PI / 180;
-    }
-
-    public double distanceInKmBetweenEarthCoordinates(double lat1,double lon1,double lat2,double lon2) {
-        int earthRadiusKm = 6371;
-
-        double dLat = degreesToRadians(lat2-lat1);
-        double dLon = degreesToRadians(lon2-lon1);
-
-        lat1 = degreesToRadians(lat1);
-        lat2 = degreesToRadians(lat2);
-
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return earthRadiusKm * c;
-    }
 }
--- app/src/main/java/com/example/wbudyapp/functions/GpsTracker.java
+++ app/src/main/java/com/example/wbudyapp/functions/GpsTracker.java
@@ -1,4 +1,7 @@
 package com.example.wbudyapp.functions;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -7,9 +10,14 @@ import android.location.LocationManager;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.example.wbudyapp.R;
 
 import static android.content.Context.LOCATION_SERVICE;
 
@@ -17,7 +25,7 @@ import static android.content.Context.LOCATION_SERVICE;
  * Created by shiva on 8/4/17.
  */
 
-public class GpsTracker implements LocationListener {
+public class GpsTracker extends Activity implements LocationListener {
 
     Context context;
 
@@ -49,6 +57,40 @@ public class GpsTracker implements LocationListener {
         return null;
     }
 
+    public double getDistanceToTUL(Location l){
+        double distance=0;
+
+
+        if( l == null){
+            Toast.makeText(getApplicationContext(),"GPS unable to get Value",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            distance = distanceInKmBetweenEarthCoordinates(l.getLatitude(),l.getLongitude(),51.74720764160156,19.453283309936523);
+        }
+
+        return distance;
+    }
+
+
+    public double degreesToRadians(double degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    public double distanceInKmBetweenEarthCoordinates(double lat1,double lon1,double lat2,double lon2) {
+        int earthRadiusKm = 6371;
+
+        double dLat = degreesToRadians(lat2-lat1);
+        double dLon = degreesToRadians(lon2-lon1);
+
+        lat1 = degreesToRadians(lat1);
+        lat2 = degreesToRadians(lat2);
+
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return earthRadiusKm * c;
+    }
+
     @Override
     public void onLocationChanged(Location location) {
     }
@@ -63,5 +105,10 @@ public class GpsTracker implements LocationListener {
 
     @Override
     public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
