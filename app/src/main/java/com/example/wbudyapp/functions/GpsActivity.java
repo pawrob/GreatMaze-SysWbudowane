cc5368b76aef978a77ca25abe34ede553d2fef98
Paweł Bucki <mrpawrob@gmail.com>
2020-08-26 18:48:45 +0200

    dodanie mierzenia odleglosci

--- app/src/main/java/com/example/wbudyapp/functions/GpsActivity.java
+++ app/src/main/java/com/example/wbudyapp/functions/GpsActivity.java
@@ -5,6 +5,7 @@ import androidx.core.app.ActivityCompat;
 
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -19,8 +20,7 @@ import com.example.wbudyapp.R;
 
 public class GpsActivity extends AppCompatActivity {
     Button btnLoc;
-    private TextView longitude;
-    private TextView latitude;
+    private TextView longitude,latitude,distance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +28,7 @@ public class GpsActivity extends AppCompatActivity {
         btnLoc = (Button) findViewById(R.id.btnGetLoc);
         ActivityCompat.requestPermissions(GpsActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
         btnLoc.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
 
@@ -41,10 +42,31 @@ public class GpsActivity extends AppCompatActivity {
 //                    Toast.makeText(getApplicationContext(),"GPS Lat = "+lat+"\n lon = "+lon,Toast.LENGTH_SHORT).show();
                     longitude = findViewById(R.id.lat);
                     latitude = findViewById(R.id.lon);
+                    distance = findViewById(R.id.dystDoPl);
                     latitude.setText("Lat: "+l.getLatitude());
                     longitude.setText("Lon: "+l.getLongitude());
+                    distance.setText("Distance: " + distanceInKmBetweenEarthCoordinates(l.getLatitude(),l.getLongitude(),51.74720764160156,19.453283309936523));
                 }
             }
         });
+    }
+
+    public double degreesToRadians(double degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    public double distanceInKmBetweenEarthCoordinates(double lat1,double lon1,double lat2,double lon2) {
+        int earthRadiusKm = 6371;
+
+        double dLat = degreesToRadians(lat2-lat1);
+        double dLon = degreesToRadians(lon2-lon1);
+
+        lat1 = degreesToRadians(lat1);
+        lat2 = degreesToRadians(lat2);
+
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return earthRadiusKm * c;
     }
 }
